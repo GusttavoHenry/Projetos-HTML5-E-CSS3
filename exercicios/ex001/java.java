4c5f7c764a4b2d5d9d835483c3ac1b254a2a3668
GusttavoHenry <gustavohgs244@gmail.com>
2022-07-25 13:23:41 -0300

    Atualização de exercicios

---
+++ exercicios/ex001/java.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class java{
+
+public static void main(String[] args) {
+    JOptionPane.showMessageDialog(parentComponent, message, title, messageType);
+}
+}
